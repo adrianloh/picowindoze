0fce0f6bdafb00e8b31790b59d22652b586d2c87
adrianloh <adrianloh@gmail.com>
2024-01-31 23:16:51 +0800

    Update

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include "pico/stdlib.h"
 #include "pico/time.h"
 
-#define LED_PIN 25
+#define LED_PIN 22
 
 bool repeating_timer_callback(struct repeating_timer *t) {
     gpio_xor_mask(1u << LED_PIN);
@@ -17,21 +17,8 @@ int main() {
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
     struct repeating_timer timer;
-    add_repeating_timer_us(-500000, repeating_timer_callback, NULL, &timer);
+    add_repeating_timer_us(-200000, repeating_timer_callback, NULL, &timer);
 
-    while(1){
-        printf("%d\n", time_us_64());
-        sleep_ms(1000);
+    while(1) {
     }
-
-
-    // uint i = 0;
-    // do {
-    //     i++;
-    //     if (i % 2000001 == 0) {
-    //         i = 0;
-    //         gpio_xor_mask(1u << LED_PIN);
-    //     }
-    // } while (1);
-    // return 0;
 }
